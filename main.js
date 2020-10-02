3e802bb3890166ee75f225e5239308e6d0fe3ca3
Christian Wander <c.wander@synoa.de>
2020-10-02 22:02:18 +0200

    recreating app based on a cooler tutorial

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+const electron = require('electron')
+
+// handles application life
+const app = electron.app
+
+// creates the native browser window
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow // saves a reference to the main window
 
 function createWindow() {
-    // Create the browser window
 
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true // wtf is that?
-        }
+    mainWindow = new BrowserWindow({ width: 800, height: 600})
+
+    // what we use here is called a `template literal`
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+    mainWindow.webContents.openDevTools()
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
     })
-
-    // and load the index.html of the app.
-    console.log("Loading index html...")
-    win.loadFile('index.html')
-    
-    win.webContents.openDevTools()
 }
 
-// called when electon is initialized.
-app.whenReady().then(createWindow)
+app.on('ready', createWindow)
 
-app.on('window-all-closed', () => {
-    console.log("Deactivating window.")
+// Quit when closed, except on mac
+app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
-        app.quit() // apparently only when not on mac :D
-        // ooh ok it's conventional on Mac to stay on until I press cmd+Q
+        app.quit()
     }
-    console.log("not quitting since we're on mac.")
 })
 
-// recreate window when dock icon is clicked and now window is present.
-// also common on Mac
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        console.log("Activating the window")
+// If on mac, re-create the window when we're activated again from the dock.
+app.on('activate', function() {
+    if (mainWindow === null) {
         createWindow()
     }
 })
-
-
